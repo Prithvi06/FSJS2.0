50c0ca5da67f1d3d58cb4afe76582057d43560f7
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-02-27 23:30:49 +0530

    Add navbar code

--- React/pwskill/src/index.js
+++ React/pwskill/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Components/Header';
 import './index.css'
+import Header from './Components/Header';
+import Main from './Components/Main'
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Main />
   </React.StrictMode>
 );
 
