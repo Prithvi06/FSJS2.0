d176b40d314638ee9c6749512bf1b730b395e259
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-08 23:18:51 +0530

    Add routing

--- React/demorouter/src/index.js
+++ React/demorouter/src/index.js
@@ -2,11 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import About from './Components/About';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+
+
+const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children:[
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/about",
+          element: <About/>
+        },
+        {
+          path: "/contact",
+          element: <Contact/>
+        },
+      ],
+    }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter} />
   </React.StrictMode>
 );
 
