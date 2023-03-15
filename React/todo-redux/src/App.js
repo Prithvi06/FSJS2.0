6292738ddc11dc4ff98a5331904725a389444aad
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-15 23:46:30 +0530

    display list task

--- React/todo-redux/src/App.js
+++ React/todo-redux/src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
-import Todos from './components/Todos';
+import ListTodos from './components/ListTodos';
 import { addTodo } from './feature/todoSlice';
 import { useSelector, useDispatch } from 'react-redux'
+// import TaskForm from './components/TaskForm';
 
 function App() {
-
+  const [openForm, setopenForm] = useState(false)
   const [todo, setTodo] = useState({})
+  const [disable, setDisable] = useState(true)
   const dispatch = useDispatch()
   const updateState = (e) => {
+    if (e.target.value){
+      setDisable(false)
+    }else{
+      setDisable(true)
+    }
     setTodo(
       {
         ...todo,
@@ -16,45 +23,63 @@ function App() {
     )
   }
   const submit = (e) => {
-    e.preventDefault()
-    if (todo.heading){
-
+    if (!disable){
+      e.preventDefault()
+      setDisable(true)
       dispatch(addTodo(todo))
       console.log(todo)
+      setTodo({})
     }
+    setopenForm(false)
   }
   const todos = useSelector((state) => state.todo.todos)
 
   return (
     <div className="App">
-      <h1>TODO</h1>
-
-      <>
-        {
-            todos.map((todo) => (
-                <Todos key={todo.id} 
-                heading={todo.heading} 
-                description={todo.description}
-                id={todo.id}
-                />
-              
-          ))
-        }
-      </>
-
-      <div className='form-div border p-2'>
-        <form onSubmit={submit}>
-          <div>
-            <label className='block mb-2 text-sm font-medium text-gray-900'>Heading</label>
-            <input type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='heading' name='heading' placeholder='heading' onChange={updateState}></input>
-          </div>
-          <div>
-            <label className='block mb-2 text-sm font-medium text-gray-900'>Description</label>
-            <input type='text' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='description' name='description' placeholder='description' onChange={updateState}></input>
-          </div>
-          <button className='border rounded-lg bg-green-500 p-2 text-sm mt-2' type='submit'>Add Task</button>
-        </form>
-      </div>
+      <h1 className='text-center text-2xl font-sans font-bold mb-10 text-white'>TODO BOARD</h1>
+      <main className='flex flex-wrap gap-x-28'>
+        <div className='p-2 basis-1/4 tasks backdrop-blur-sm bg-white/30  rounded-md'>
+          <h3 className='font-bold'>Project Todos</h3>
+          <>
+            {
+              todos.map((todo) => (
+                  <ListTodos key={todo.id} 
+                  heading={todo.heading} 
+                  description={todo.description}
+                  id={todo.id}/>
+              ))
+            }
+          </>
+          <button className='text-white w-[100%] mt-3' onClick={()=> {setopenForm(true)}}>+ Add Task</button>
+          {openForm && (
+            <div className='form-div border border-gray-200 p-2 mt-3'>
+              <form onSubmit={submit}>
+                <div>
+                  <label className='block mb-1 text-sm font-medium text-gray-900'>Heading</label>
+                  <input type='text' className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='heading' name='heading' placeholder='Heading' onChange={updateState}></input>
+                </div>
+                <div>
+                  <label className='block mb-1 text-sm font-medium text-gray-900'>Description</label>
+                  <input type='text' className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='description' name='description' placeholder='Description' onChange={updateState}></input>
+                </div>
+                <div className='flex space-between'>
+                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30' disabled={disable}>Add Task</button>
+                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30'>Cancel</button>
+                </div>
+              </form>
+            </div>
+          )
+          }
+        </div>
+        <div className='p-2 basis-1/4 tasks  backdrop-blur-sm bg-white/30 rounded-md'>
+          <h3 className='font-bold'>In-progress</h3>
+          <button className='text-white w-[100%] mt-3'>+ Add Task</button>
+        </div>
+        <div className='p-2 basis-1/4 tasks backdrop-blur-sm bg-white/30 rounded-md'>
+          <h3 className='font-bold'>Completed</h3>
+          <button className='text-white w-[100%] mt-3'>+ Add Task</button>
+        </div>
+      </main>
     </div>
   )
 }
