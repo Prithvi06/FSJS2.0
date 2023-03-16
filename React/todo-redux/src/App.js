5b1867c518b3f8ad03d28d004822f49797bacc5b
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-16 23:31:32 +0530

    Add update button with form state

--- React/todo-redux/src/App.js
+++ React/todo-redux/src/App.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 import ListTodos from './components/ListTodos';
-import { addTodo } from './feature/todoSlice';
+import { addTodo, editTodo } from './feature/todoSlice';
 import { useSelector, useDispatch } from 'react-redux'
+
 // import TaskForm from './components/TaskForm';
 
 function App() {
   const [openForm, setopenForm] = useState(false)
   const [todo, setTodo] = useState({})
   const [disable, setDisable] = useState(true)
+  const [editheading, setHeading] = useState("")
+  const [editdescrip, setDescription] = useState("")
+  const [button, setButton] = useState("Add Task")
+  const [todokey, setTodoKey] = useState()
+
   const dispatch = useDispatch()
   const updateState = (e) => {
     if (e.target.value){
       setDisable(false)
+      if (e.target.name === "heading"){
+        setHeading(e.target.value)
+      }if (e.target.name === "description"){
+        setDescription(e.target.value)
+      }
     }else{
       setDisable(true)
     }
@@ -23,12 +34,26 @@ function App() {
     )
   }
   const submit = (e) => {
-    if (!disable){
-      e.preventDefault()
+    e.preventDefault()
+    if (e.target.innerText === "Cancel"){
+        setDisable(true);
+        setTodo({});
+    }else if (!disable && e.target.innerText==="Add Task"){
       setDisable(true)
       dispatch(addTodo(todo))
       console.log(todo)
       setTodo({})
+      setHeading("")
+      setDescription("")
+    }else if (!disable && e.target.innerText==="Update Task"){
+      let updatetodo = {
+        "id": todokey,
+        "heading": editheading,
+        "description": editdescrip
+      }
+      dispatch(editTodo(updatetodo));
+      setHeading("")
+      setDescription("")
     }
     setopenForm(false)
   }
@@ -46,25 +71,31 @@ function App() {
                   <ListTodos key={todo.id} 
                   heading={todo.heading} 
                   description={todo.description}
-                  id={todo.id}/>
+                  id={todo.id}
+                  setHeading={setHeading}
+                  setDescription={setDescription}
+                  setopenForm={setopenForm}
+                  setButton={setButton}
+                  setTodoKey={setTodoKey}
+                  />
               ))
             }
           </>
           <button className='text-white w-[100%] mt-3' onClick={()=> {setopenForm(true)}}>+ Add Task</button>
           {openForm && (
             <div className='form-div border border-gray-200 p-2 mt-3'>
-              <form onSubmit={submit}>
+              <form>
                 <div>
                   <label className='block mb-1 text-sm font-medium text-gray-900'>Heading</label>
-                  <input type='text' className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='heading' name='heading' placeholder='Heading' onChange={updateState}></input>
+                  <input type='text' value={editheading} className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='heading' name='heading' placeholder='Heading' onChange={updateState}></input>
                 </div>
                 <div>
                   <label className='block mb-1 text-sm font-medium text-gray-900'>Description</label>
-                  <input type='text' className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='description' name='description' placeholder='Description' onChange={updateState}></input>
+                  <input type='text' value={editdescrip} className='w-[100%] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2' id='description' name='description' placeholder='Description' onChange={updateState}></input>
                 </div>
-                <div className='flex space-between'>
-                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30' disabled={disable}>Add Task</button>
-                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30'>Cancel</button>
+                <div className='flex justify-around'>
+                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30 hover:bg-blue-600 hover:text-white' disabled={disable} onClick={submit}>{button}</button>
+                <button className='border rounded-full px-2 py-1 text-sm mt-2 backdrop-blur-sm bg-white/30 hover:bg-blue-600 hover:text-white' onClick={submit}>Cancel</button>
                 </div>
               </form>
             </div>
