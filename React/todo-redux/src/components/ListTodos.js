1477546f293414a24d912afe89a6d1f9588fb350
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-16 23:31:56 +0530

    Show todo list after update

--- React/todo-redux/src/components/ListTodos.js
+++ React/todo-redux/src/components/ListTodos.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { useDispatch } from 'react-redux'
 import { removeTodo } from "../feature/todoSlice";
-// import { addTodo,removeTodo} from "../feature/todoSlice";
 
 function ListTodos(props){
-    // const todos = useSelector((state) => state.todo.todos)
     const dispatch = useDispatch()
+
     const remove = (id) => {
-        dispatch(removeTodo(id))
+      dispatch(removeTodo(id))
     }
+
+    const updateTodo = (id, heading, description) => {
+      props.setHeading(heading)
+      props.setDescription(description)
+      props.setButton("Update Task")
+      props.setopenForm(true)
+      props.setTodoKey(id)
+    }
+
     if (props.id){
     return(
         <div className="bg-white m-1 p-2 rounded-md">
-          <p>
-            <b className="text-violet-600">{props.heading}</b>
-            <span className="text-rose-600 float-right cursor-pointer hover:text-rose-700" onClick={() => {remove(props.id)}}> <b>X</b></span>
-          </p>
+          <div className="flex justify-between">
+            <h5 className="text-violet-600 font-bold">{props.heading}</h5>
+            <p className="flex gap-5">
+              <span className="text-blue-500  cursor-pointer hover:text-blue-700" onClick={() => {updateTodo(props.id, props.heading, props.description)}}> <i className="fa-solid fa-pen"></i> </span>
+              <span className="text-rose-500 float-right cursor-pointer hover:text-rose-700" onClick={() => {remove(props.id)}}> <i className="fa-solid fa-trash"></i></span>
+            </p>
+          </div>
           <p>{props.description}</p>
         </div>
     )}
