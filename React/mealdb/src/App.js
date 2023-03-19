682c7d04ea25286535b975282ac5cfc5bef77375
Anirudh Jwala <anirudhsai2@gmail.com>
2023-03-19 21:06:02 +0530

    MealsDB Project updated
    Proxy removed
    Refactored App.jsx and Card.js file

--- React/mealdb/src/App.js
+++ React/mealdb/src/App.js
@@ -2,34 +2,43 @@ import React, { useState } from 'react';
 import MealCard from './components/Card';
 
 function App() {
-  const [mealName, setmealName] = useState("")
-  const [mealData, setMealData] = useState({})
+  const [mealName, setmealName] = useState('');
+  const [mealData, setMealData] = useState([]);
 
-  const mealState = (e) => {
-      setmealName(e.target.value)
-  }
+  const handleMealNameChange = (e) => {
+    setmealName(e.target.value);
+  };
+
   const searchMeal = (e) => {
-    console.log("seach")
-    e.preventDefault()
-    var url = "www.themealdb.com/api/json/v1/1/search.php?s=" + mealName
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {setMealData(data);console.log(data)})
-    .catch(error => console.error(error))
-  }
-
+    e.preventDefault();
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data.meals);
+      })
+      .catch((error) => console.error(error));
+  };
 
   return (
-    <div className='input-div'>
-      <form>
-        <input type="text" className="meal-input" placeholder="Search your meal" onChange={mealState}/>
-        <button onClick={searchMeal} className="search-btn">Search</button>
-      </form>
+    <>
+      <div className="input-div">
+        <form>
+          <input
+            type="text"
+            className="meal-input"
+            placeholder="Search your meal"
+            onChange={handleMealNameChange}
+          />
 
-      {mealData && <MealCard mealData={mealData} />}
-    </div>
+          <button onClick={searchMeal} className="search-btn">
+            Search
+          </button>
+        </form>
+      </div>
 
-  )
+      <MealCard mealData={mealData} />
+    </>
+  );
 }
 
 export default App;
