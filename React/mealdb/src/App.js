32379f860d64f1a986e0a9f8db4a75bfa2ad3023
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-19 18:26:26 +0530

    Add meal apps file

---
+++ React/mealdb/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [mealName, setmealName] = useState("")
+
+  const mealState = (e) => {
+      setmealName(e.target.value)
+  }
+  const searchMeal = (e) => {
+    console.log("seach")
+    e.preventDefault()
+    var url = "www.themealdb.com/api/json/v1/1/search.php?s=" + mealName
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+  }
+
+
+  return (
+    <div className='input-div'>
+      <form>
+        <input type="text" className="meal-input" placeholder="Search your meal" onChange={mealState}/>
+        <button onClick={searchMeal} className="search-btn">Search</button>
+      </form>
+    </div>
+
+  )
+}
+
+export default App;
