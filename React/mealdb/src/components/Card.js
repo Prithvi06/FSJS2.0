2da29e8c30b8d50ba26de0616547297a7b2a0202
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-03-22 00:52:57 +0530

    Add css

--- React/mealdb/src/components/Card.js
+++ React/mealdb/src/components/Card.js
@@ -5,12 +5,12 @@ const MealCard = ({ mealData }) => {
         mealData.map((meal) => <div className="card" key={meal.strMeal}>
                   <img src={meal.strMealThumb} className="mealtheme" alt="meal thumb"></img>
                   <div className="meal-info">
-                    <h3>Meal Name:</h3>
-                    <p><b>{meal.strMeal}</b></p>
-                  </div>
-                  <div className="meal-info">
                     <h3>Category:</h3>
                     <p><b>{meal.strCategory}</b></p>
+                  </div>
+                  <div className="meal-info">
+                    <h3>Meal Name:</h3>
+                    <p><b>{meal.strMeal}</b></p>
                   </div>
                 </div>
               )
