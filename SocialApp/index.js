6be51b045f701d7dfe48ff4c67cbc068349a66b8
prithviraj <prithvirajsinghthakur74@gmail.com>
2023-04-01 16:43:58 +0530

    Create first express app

--- SocialApp/index.js
+++ SocialApp/index.js
@@ -1,1 +1,18 @@
-console.log("hello node");
+const express = require("express")
+
+const app = express()
+
+const PORT = 3000
+
+app.get("/", (req, res) => {
+    const data = {
+        "name": "prithvi",
+        "follow": 300,
+        "follower": 600
+    }
+    res.status(200).send(data)
+})
+
+app.listen(PORT, () => {
+    console.log("Server Running")
+})
